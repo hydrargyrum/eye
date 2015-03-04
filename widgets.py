e9d3c84787e922b0cf93e81d17ade10063920860
Hg <dev@indigo.re>
2015-03-04 08:30:43 +0100

    widgets: add a LogWidget for app messages

--- widgets.py
+++ widgets.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-import sys, os
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.Qsci import *
 Signal = pyqtSignal
 Slot = pyqtSlot
+
+import sys, os
+import logging
 
 from app import qApp
 import utils
@@ -253,6 +255,28 @@ class TabWidget(QTabWidget, CategoryMixin):
 	def _changeTabBarVisibility(self):
 		visible = (self.count() > 1)
 		self.tabBar().setVisible(visible)
+
+
+class LogWidget(QPlainTextEdit):
+	class LogHandler(logging.Handler):
+		def __init__(self, widget):
+			logging.Handler.__init__(self)
+			self.widget = widget
+			self.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+
+		def emit(self, record):
+			self.widget.appendPlainText(self.format(record))
+
+	def __init__(self, parent=None):
+		QPlainTextEdit.__init__(self, parent)
+		self.handler = LogWidget.LogHandler(self)
+		self.setReadOnly(True)
+
+	def install(self):
+		qApp().logger.addHandler(self.handler)
+
+	def uninstall(self):
+		qApp().logger.removeHandler(self.handler)
 
 
 class WindowRegistry(QObject):
