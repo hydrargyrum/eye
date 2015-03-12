b7c52915fd1fea1fd8827530e8cacf1b6291c2d4
Hg <dev@indigo.re>
2015-03-12 21:42:22 +0100

    add "line bookmark" helpers

--- helpers.py
+++ helpers.py
@@ -25,3 +25,26 @@ def linesMarginWidth(ed):
 		# add one character width as it may be truncated
 		ed.margins['lines'].setWidth('0%d' % ed.lines())
 
+def addBookmark(ed):
+	ln = ed.getCursorPosition()[0]
+
+	marker = ed.markers['bookmark']  
+	if marker.isAt(ln):
+		marker.removeAt(ln)
+	else:
+		marker.putAt(ln)
+
+def nextBookmark(ed):
+        ln = ed.getCursorPosition()[0]
+
+        ln = ed.markers['bookmark'].getNext(ln + 1)
+        if ln < 0:
+                ln = ed.markers['bookmark'].getNext(0)
+        ed.setCursorPosition(ln, 0)
+
+def previousBookmark(ed):
+        ln = ed.getCursorPosition()[0]
+
+        ln = ed.markers['bookmark'].getPrevious(ln - 1)
+        if ln < 0:
+                ln = ed.markers['bookmark'].getPrevious(ed.lines())
