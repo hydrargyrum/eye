47134b0de8cecfafc83d7b5caba1b1a8ae2901f1
Hg <dev@indigo.re>
2015-03-22 11:16:42 +0100

    introduce utils.PropDict that treats attributes and keys the same

--- utils.py
+++ utils.py
@@ -5,6 +5,9 @@ import contextlib
 import tempfile
 import re
 
+__all__ = ('exceptionLogging', 'writeBytesToFile', 'readBytesFromFile',
+           'parseFilename', 'PropDict')
+
 @contextlib.contextmanager
 def exceptionLogging(reraise=True):
 	try:
@@ -49,3 +52,18 @@ def parseFilename(filepath):
 	filepath = filepath[:mtc.start()]
 
 	return (filepath, row, col)
+
+
+class PropDict(dict):
+	def __getattr__(self, k):
+		try:
+			return self[k]
+		except KeyError:
+			# raised so getattr with a default value works
+			raise AttributeError('object has no attribute %r' % k)
+
+	def __setattr__(self, k, v):
+		self[k] = v
+
+	def __delattr__(self, k):
+		del self[k]
