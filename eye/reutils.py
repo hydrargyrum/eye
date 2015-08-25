c1deea9f8649a0ce90c7a61bb87951d25363dbda
Hg <dev@indigo.re>
2015-08-25 22:42:22 +0200

    reutils: support wildcard to regexp

--- eye/reutils.py
+++ eye/reutils.py
@@ -16,11 +16,52 @@ def qtEnumToCs(enum):
 	return enum == Qt.CaseSensitive
 
 
+def glob2re(globstr, can_escape=False, explicit_slash=True, explicit_dot=True,
+            exact=False):
+	# fnmatch.translate uses python-specific syntax
+	dot = '[^/]' if explicit_slash else '.'
+	first_dot = '[^/.]' if explicit_slash else '[^.]'
+
+	def is_first_component(mtc):
+		return (mtc.start() == 0
+		     or mtc.string[mtc.start() - 1] == '/')
+
+	def replace(mtc):
+		s = mtc.group(0)
+		if s == '?':
+			return first_dot if is_first_component(mtc) else dot
+		elif s == '*':
+			p = first_dot if is_first_component(mtc) else dot
+			return p + '*'
+		elif s.startswith('['):
+			return s
+		elif can_escape and s == '\\\\':
+			return s
+		elif can_escape and s.startswith('\\'):
+			return s
+		elif s in '()[]{}.^$+\\':
+			return r'\%s' % s
+		else:
+			return s
+
+	if can_escape:
+		r = re.sub(r'\?|\*|\[[^]]*\]|\\\\|\\.|.', replace, globstr)
+	else:
+		r = re.sub(r'\?|\*|\[[^]]*\]|.', replace, globstr)
+	if exact:
+		r = '^%s$' % r
+	return r
+
+
 def qreToPattern(qre):
+	s = qre.pattern()
+
 	if qre.patternSyntax() == qre.FixedString:
-		return qre.escape(qre.pattern())
-	if qre.patternSyntax() in (qre.RegExp, qre.RegExp2):
-		return qre.pattern()
+		return qre.escape(s)
+	elif qre.patternSyntax() in (qre.RegExp, qre.RegExp2):
+		return s
+	elif qre.patternSyntax() == qre.Wildcard:
+		return glob2re(s)
+	elif qre.patternSyntax() == qre.WildcardUnix:
+		return glob2re(can_escape=True)
 	raise NotImplementedError()
-	# TODO handle wildcard
-	#~ if qre.patternSyntax() == qre.WildcardUnix:
