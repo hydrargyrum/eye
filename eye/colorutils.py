ce76a2fcca0bcd3ad7bdeaeb0317de8c44138bcc
Hg <dev@indigo.re>
2015-08-27 20:34:30 +0200

    colorutils: support #RRGGBB and named color format

--- eye/colorutils.py
+++ eye/colorutils.py
@@ -4,13 +4,17 @@ from PyQt4.QtGui import QColor
 __all__ = ('QColorAlpha', 'QColor')
 
 def QColorAlpha(*args):
-	if len(args) == 2:
+	if len(args) == 1 and isinstance(args[0], (str, unicode)):
+		s = args[0]
+		if s.startswith('#') and len(s) == 9: #RRGGBBAA
+			qc = QColor(s[:7])
+			qc.setAlpha(int(s[7:], 16))
+		else:
+			qc = QColor(s)
+		return qc
+	elif len(args) == 2:
 		qc = QColor(args[0])
 		qc.setAlpha(args[1])
 		return qc
 	elif len(args) >= 3:
 		return QColor(*args)
-	elif isinstance(args[0], str) and args[0].startswith('#'):
-		qc = QColor(args[0][:7])
-		qc.setAlpha(int(args[0][7:], 16))
-		return qc
