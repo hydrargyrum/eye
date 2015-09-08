c253261da12f61bd79461c799d126b1614543411
Hg <dev@indigo.re>
2015-09-09 00:11:03 +0200

    reutils: add unit tests for glob2re

--- eye/reutils.py
+++ eye/reutils.py
@@ -2,6 +2,7 @@
 
 from PyQt4.QtCore import QRegExp, Qt
 import re
+import unittest
 
 
 __all__ = ('csToQtEnum', 'qtEnumToCs', 'qreToPattern')
@@ -103,3 +104,175 @@ def qreToPattern(qre):
 		return glob2re(s, dotslash=DOTSLASH_NO_SLASH_AND_HIDDEN,
 		               can_escape=True)
 	raise NotImplementedError()
+
+
+class ReTests(unittest.TestCase):
+	def check_pattern(self, glob, matches, non_matches, **options):
+		pattern = glob2re(glob, exact=True, **options)
+		r = re.compile(pattern)
+		for i in matches:
+			msg = '%s should match %s (%s) %r' % (glob, i, pattern, options)
+			self.assertIsNotNone(r.match(i), msg)
+		for i in non_matches:
+			msg = '%s should not match %s (%s) %r' % (glob, i, pattern, options)
+			self.assertIsNone(r.match(i), msg)
+
+	def test_glob2re_generic(self):
+		options = dict(dotslash=0)
+		self.check_pattern(
+			'*',
+			'foo .foo foo/bar foo.bar [] *'.split(),
+			[],
+			**options)
+		self.check_pattern(
+			'*/*',
+			'foo/bar foo/.bar foo/foo/bar foo/bar.baz foo/*'.split(),
+			'foo.bar foo'.split(),
+			**options)
+		self.check_pattern(
+			'foo*',
+			'foo foobar foo.bar foo/bar foobar/'.split(),
+			'bar barfoo .foo '.split(),
+			**options)
+		self.check_pattern(
+			'*bar',
+			'bar .bar foobar foo.bar foo/bar .foobar'.split(),
+			'barfoo'.split(),
+			**options)
+		self.check_pattern(
+			'*bar*',
+			'bar .bar foobar foo/bar barbaz foobarbaz foo.bar bar.baz'.split(),
+			[],
+			**options)
+		self.check_pattern(
+			'???',
+			'foo .fo fo/ f/o /fo'.split(),
+			'foobar'.split(),
+			**options)
+		self.check_pattern(
+			'?/?',
+			'f/b f/. ./b'.split(),
+			'foo'.split(),
+			**options)
+
+	def test_glob2re_filename(self):
+		self.check_pattern(
+			'*',
+			'foo foo.bar [] *'.split(),
+			'.foo foo/bar'.split())
+		self.check_pattern(
+			'*/*',
+			'foo/bar foo/bar.baz foo/*'.split(),
+			'foo/.bar foo.bar foo/foo/bar foo'.split())
+		self.check_pattern(
+			'foo*',
+			'foo foobar foo.bar'.split(),
+			'bar barfoo .foo foo/bar foobar/'.split())
+		self.check_pattern(
+			'*bar',
+			'bar foobar foo.bar'.split(),
+			'barfoo .bar .foobar foo/bar'.split())
+		self.check_pattern(
+			'*bar*',
+			'bar foobar barbaz foobarbaz foo.bar bar.baz'.split(),
+			'.bar foo/bar'.split())
+		self.check_pattern(
+			'???',
+			'foo'.split(),
+			'.fo fo/ f/o /fo foobar'.split())
+		self.check_pattern(
+			'?/?',
+			'f/b'.split(),
+			'foo f/. ./b'.split())
+
+	def test_glob2re_recursive(self):
+		options = dict(double_star=True)
+		self.check_pattern(
+			'**/*',
+			'foo foo/foo foo/foo/bar'.split(),
+			'.foo foo/.foo'.split(),
+			**options)
+		self.check_pattern(
+			'**/foo',
+			'foo foo/foo'.split(),
+			'.foo foo/.foo foo/foo/bar bar'.split(),
+			**options)
+		self.check_pattern(
+			'foo/**/bar',
+			'foo/bar foo/baz/bar foo/baz/baz/bar'.split(),
+			'.foo foo/.bar bar baz/bar foo/baz'.split(),
+			**options)
+		self.check_pattern(
+			'foo/**',
+			'foo foo/bar foo/baz/bar foo/.bar'.split(),
+			'.foo bar baz/bar'.split(),
+			**options)
+
+	def test_glob2re_escapable(self):
+		options = dict(can_escape=True)
+		self.check_pattern(
+			r'\*',
+			r'\*'.split(),
+			r'foo *'.split())
+		self.check_pattern(
+			r'\*',
+			r'*'.split(),
+			r'\* foo ?'.split(),
+			**options)
+
+		self.check_pattern(
+			r'\?',
+			'?'.split(),
+			r'* f \?'.split(),
+			**options)
+		self.check_pattern(
+			r'\[a\]',
+			'[a]'.split(),
+			'a [] ['.split(),
+			**options)
+
+		self.check_pattern(
+			r'foo\\bar',
+			r'foo\\bar'.split(),
+			r'foobar foo\bar'.split())
+		self.check_pattern(
+			r'foo\\bar',
+			r'foo\bar'.split(),
+			r'foo\\bar foobar'.split(),
+			**options)
+
+	def test_glob2re_charset(self):
+		options = dict()
+		self.check_pattern(
+			'[ac]b[d-l]',
+			'abd cbl abe cbh'.split(),
+			'dbd bbd cbt abdx a'.split())
+		self.check_pattern(
+			'[ac-eh-k]',
+			'a c d e h i j k'.split(),
+			'* b f g l o z [ [ac-eh-k] aa'.split(),
+			**options)
+		self.check_pattern(
+			'[!a-et]',
+			'f x *'.split(),
+			'a e c t xx'.split(),
+			**options)
+		self.check_pattern(
+			'[a-m]*',
+			'a bn m moooooo'.split(),
+			'x xa *'.split(),
+			**options)
+		self.check_pattern(
+			'[',
+			'['.split(),
+			'x xa *'.split(),
+			**options)
+		self.check_pattern(
+			'[]',
+			[],
+			'x xa *'.split(),
+			**options)
+
+
+if __name__ == '__main__':
+	unittest.main()
