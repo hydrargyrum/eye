82cccc8c4f67bcfa1a52540c138d8b61cfb187c1
Hg <dev@indigo.re>
2015-09-18 22:41:06 +0200

    colorutils: QColorAlpha supports a single non-string argument

--- eye/colorutils.py
+++ eye/colorutils.py
@@ -5,17 +5,19 @@ from PyQt4.QtGui import QColor
 __all__ = ('QColorAlpha', 'QColor')
 
 def QColorAlpha(*args):
-	if len(args) == 1 and isinstance(args[0], (str, unicode)):
-		s = args[0]
-		if s.startswith('#') and len(s) == 9: #RRGGBBAA
-			qc = QColor(s[:7])
-			qc.setAlpha(int(s[7:], 16))
-		else:
-			qc = QColor(s)
-		return qc
-	elif len(args) == 2:
+	if len(args) == 1:
+		if isinstance(args[0], (str, unicode)):
+			s = args[0]
+			if s.startswith('#') and len(s) == 9: #RRGGBBAA
+				qc = QColor(s[:7])
+				qc.setAlpha(int(s[7:], 16))
+				return
+			else: # #RRGGBB, "red"
+				return QColor(s)
+		return QColor(args[0]) # Qt.red
+	elif len(args) == 2: # (Qt.red, alpha)
 		qc = QColor(args[0])
 		qc.setAlpha(args[1])
 		return qc
-	elif len(args) >= 3:
+	elif len(args) >= 3: # (r, g, b)
 		return QColor(*args)
