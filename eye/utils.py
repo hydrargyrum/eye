f7599b3b131abcb5433baf086b577f615a194f07
Hg <dev@indigo.re>
2015-09-22 17:49:04 +0200

    utils: add a ignoreExceptions decorator

--- eye/utils.py
+++ eye/utils.py
@@ -1,10 +1,11 @@
 # this project is licensed under the WTFPLv2, see COPYING.txt for details
 
 from contextlib import contextmanager
+from functools import wraps
 import logging
 from PyQt4.QtGui import QApplication
 
-__all__ = ('exceptionLogging',)
+__all__ = ('exceptionLogging', 'ignoreExceptions')
 
 @contextmanager
 def exceptionLogging(reraise=True, logger=None, level=logging.ERROR):
@@ -17,3 +18,20 @@ def exceptionLogging(reraise=True, logger=None, level=logging.ERROR):
 		logger.log(level, 'an exception occured', exc_info=True)
 		if reraise:
 			raise
+
+
+def ignoreExceptions(return_value, logger=None, level=logging.ERROR):
+	def caller(f):
+		@wraps(f)
+		def decorator(*a, **kw):
+			try:
+				return f(*a, **kw)
+			except Exception:
+				if logger is None:
+					logger = QApplication.instance().logger
+				logger.log(level, 'an exception occured when calling %r', f, exc_info=True)
+				return return_value
+
+		return decorator
+
+	return caller
