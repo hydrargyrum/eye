ced192bf53290e55d0dd52ab7387f22493635547
Hg <dev@indigo.re>
2015-09-22 17:45:28 +0200

    utils: exceptionLogging takes a level parameter and avoid BaseException

--- eye/utils.py
+++ eye/utils.py
@@ -1,18 +1,19 @@
 # this project is licensed under the WTFPLv2, see COPYING.txt for details
 
 from contextlib import contextmanager
+import logging
 from PyQt4.QtGui import QApplication
 
 __all__ = ('exceptionLogging',)
 
 @contextmanager
-def exceptionLogging(reraise=True, logger=None):
+def exceptionLogging(reraise=True, logger=None, level=logging.ERROR):
 	try:
 		yield
-	except BaseException, e:
+	except Exception:
 		# could guess logger by inspecting traceback
 		if logger is None:
 			logger = QApplication.instance().logger
-		logger.exception(e)
+		logger.log(level, 'an exception occured', exc_info=True)
 		if reraise:
 			raise
