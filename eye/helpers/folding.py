e4292b6da42b099b281ac152c3f5bfb8bfd1355e
Hg <dev@indigo.re>
2016-04-24 10:57:01 +0200

    helpers.folding: use a timer to avoid refolding for every character

--- eye/helpers/folding.py
+++ eye/helpers/folding.py
@@ -1,6 +1,6 @@
 # this project is licensed under the WTFPLv2, see COPYING.txt for details
 
-from PyQt5.QtCore import QObject, pyqtSignal as Signal, pyqtSlot as Slot
+from PyQt5.QtCore import QObject, QTimer, pyqtSignal as Signal, pyqtSlot as Slot
 from PyQt5.Qsci import QsciScintilla
 
 import re
@@ -21,17 +21,23 @@ def disableLexerFolding(ed, *args):
 class MarkerFolder(QObject, HasWeakEditorMixin):
 	markerStart = re.compile(r'\{\{\{')
 	markerEnd = re.compile(r'\}\}\}')
+	interval = 100
 
 	def __init__(self, editor=None, **kwargs):
 		super(MarkerFolder, self).__init__(**kwargs)
 		self.editor = editor
 		editor.sciModified.connect(self.onModification)
+		self.timer = QTimer()
+		self.timer.setSingleShot(True)
+		self.timer.timeout.connect(self.refold)
+		self.start = 0
 
-	def refold(self, start=0):
-		n = self.editor.getFoldLevel(start) & QsciScintilla.SC_FOLDLEVELNUMBERMASK
+	@Slot()
+	def refold(self):
+		n = self.editor.getFoldLevel(self.start) & QsciScintilla.SC_FOLDLEVELNUMBERMASK
 
 		first = True
-		for i in range(start, self.editor.lines()):
+		for i in range(self.start, self.editor.lines()):
 			if not first:
 				self.editor.setFoldLevel(i, n)
 			first = False
@@ -45,11 +51,19 @@ class MarkerFolder(QObject, HasWeakEditorMixin):
 
 	@Slot(object)
 	def onModification(self, st):
+		refold = None
+
 		pos, ev, text, length, added, line, folda, foldb, tok, anno = st
 		if ev & QsciScintilla.SC_MOD_INSERTTEXT:
-			line, col = self.editor.lineIndexFromPosition(pos)
-			self.refold(line)
+			refold, _ = self.editor.lineIndexFromPosition(pos)
 		elif ev & QsciScintilla.SC_MOD_DELETETEXT:
-			line, col = self.editor.lineIndexFromPosition(pos)
-			self.refold(line)
+			refold, _ = self.editor.lineIndexFromPosition(pos)
+
+		if refold is not None:
+			if self.timer.isActive():
+				self.start = min(self.start, refold)
+			else:
+				self.start = refold
+				self.timer.start(self.interval)
+
 		# TODO smarter refold: check if insert/delete contains pattern or changes folding
