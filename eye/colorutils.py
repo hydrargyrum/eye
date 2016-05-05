65963aa44082271de843571307b5ae3bd1895da5
Hg <dev@indigo.re>
2016-05-05 22:44:24 +0200

    colorutils: fix #RRGGBBAA case and add tests

--- eye/colorutils.py
+++ eye/colorutils.py
@@ -2,6 +2,9 @@
 
 from __future__ import unicode_literals
 
+import unittest
+
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QColor
 
 from .three import bytes, str
@@ -15,7 +18,7 @@ def QColorAlpha(*args):
 			if s.startswith('#') and len(s) == 9: #RRGGBBAA
 				qc = QColor(s[:7])
 				qc.setAlpha(int(s[7:], 16))
-				return
+				return qc
 			else: # #RRGGBB, "red"
 				return QColor(s)
 		return QColor(args[0]) # Qt.red
@@ -25,3 +28,22 @@ def QColorAlpha(*args):
 		return qc
 	elif len(args) >= 3: # (r, g, b)
 		return QColor(*args)
+
+
+class ColorTests(unittest.TestCase):
+	def test_normal(self):
+		self.assertEqual(QColorAlpha(Qt.red), QColor(Qt.red))
+		self.assertEqual(QColorAlpha('#123456'), QColor('#123456'))
+		self.assertEqual(QColorAlpha(1, 2, 3), QColor(1, 2, 3))
+
+	def test_alpha(self):
+		c = QColor(Qt.red)
+		c.setAlpha(128)
+		self.assertEqual(QColorAlpha(Qt.red, 128), c)
+		self.assertEqual(QColorAlpha('#ff0000', 128), c)
+		self.assertEqual(QColorAlpha('#ff000080'), c)
+		self.assertEqual(QColorAlpha(255, 0, 0, 128), c)
+
+
+if __name__ == '__main__':
+	unittest.main()
