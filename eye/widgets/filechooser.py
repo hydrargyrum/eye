8acb8fdc1788f7ff28be9587523c12a5d17f2d97
Hg <dev@indigo.re>
2016-05-17 00:18:57 +0200

    widgets.filechooser: add SubSequenceFileChooser

--- eye/widgets/filechooser.py
+++ eye/widgets/filechooser.py
@@ -1,11 +1,13 @@
 # this project is licensed under the WTFPLv2, see COPYING.txt for details
 
-from PyQt5.QtCore import QSortFilterProxyModel, QModelIndex, QRegExp, pyqtSignal, pyqtSlot, Qt
+from PyQt5.QtCore import QSortFilterProxyModel, QModelIndex, QRegExp, pyqtSignal, pyqtSlot, Qt, QTimer
+from PyQt5.QtGui import QStandardItem, QStandardItemModel
 from PyQt5.QtWidgets import QVBoxLayout, QLineEdit, QTreeView, QWidget, QFileSystemModel
 Signal = pyqtSignal
 Slot = pyqtSlot
 
 import os
+import re
 
 from ..three import str
 from ..structs import PropDict
@@ -13,7 +15,7 @@ from .helpers import WidgetMixin
 from ..helpers import buffers
 
 
-__all__ = ('FileChooser',)
+__all__ = ('FileChooser', 'SubSequenceFileChooser')
 
 
 def commonPrefix(strings):
@@ -50,9 +52,9 @@ class RootChangerProxy(QSortFilterProxyModel):
 		return self.index(srcIdx.row(), srcIdx.column(),proxyParent)
 
 
-class FileChooser(QWidget, WidgetMixin):
+class BaseFileChooser(QWidget, WidgetMixin):
 	def __init__(self, **kwargs):
-		super(FileChooser, self).__init__(**kwargs)
+		super(BaseFileChooser, self).__init__(**kwargs)
 
 		self.options = PropDict()
 
@@ -62,11 +64,147 @@ class FileChooser(QWidget, WidgetMixin):
 
 		self.edit = QLineEdit()
 		layout.addWidget(self.edit)
-		self.edit.textEdited.connect(self._onTextEdited)
 		self.setFocusProxy(self.edit)
 
 		self.view = QTreeView()
 		layout.addWidget(self.view)
+
+	def setModel(self, model):
+		self.view.setModel(model)
+
+	@Slot(str)
+	def setRoot(self, path):
+		raise NotImplementedError()
+
+
+def walk_files(root, ignore_re=None):
+	for dp, dirs, files in os.walk(root):
+		if ignore_re:
+			dirs[:] = filter(ignore_re.match, dirs)
+
+		for f in files:
+			if ignore_re and ignore_re.match(f):
+				continue
+			yield os.path.join(dp, f)
+
+
+class SubSequenceProxy(QSortFilterProxyModel):
+	def __init__(self, **kwargs):
+		super(SubSequenceProxy, self).__init__(**kwargs)
+		self.reobj = re.compile('')
+		self.cache = {}
+
+	def setFilter(self, text):
+		parts = map(QRegExp.escape, text)
+		pattern = '.*'.join('(%s)' % part for part in parts)
+		self.reobj = re.compile(pattern, re.I)
+		self.cache = {}
+		self.invalidate()
+
+	def filterAcceptsRow(self, row, parent):
+		if not self.reobj.pattern:
+			return False
+
+		mdl = self.sourceModel()
+		qidx = mdl.index(row, 0, parent)
+		text = mdl.data(qidx)
+		mtc = self.reobj.search(text)
+		if mtc:
+			self._scoreMatch(mtc, text)
+		return bool(mtc)
+
+	def _score(self, qidx):
+		text = self.sourceModel().data(qidx)
+		try:
+			return self.cache[text]
+		except KeyError:
+			pass
+
+		mtc = self.reobj.search(text)
+		return self._scoreMatch(mtc, text)
+
+	def _scoreMatch(self, mtc, text):
+		n = self.reobj.groups
+
+		seq = 0
+		sub = 0
+		for i in xrange(1, n + 1):
+			if i > 1:
+				seq += int(bool(mtc.start(i) - mtc.start(i - 1) - 1))
+
+			if mtc.start(i) == 0:
+				sub += 1
+			else:
+				sub += int(text[mtc.start(i) - 1] == '/')
+
+		score = seq - sub
+		self.cache[text] = score
+		return score
+
+	def lessThan(self, qidx1, qidx2):
+		if not self.reobj.groups:
+			return qidx1.data() < qidx2.data()
+		return self._score(qidx1) < self._score(qidx2)
+
+
+class SubSequenceFileChooser(BaseFileChooser):
+	elements_per_crawl = 20
+
+	def __init__(self, **kwargs):
+		super(SubSequenceFileChooser, self).__init__(**kwargs)
+
+		self.mdl = QStandardItemModel()
+		self.filter = SubSequenceProxy()
+		self.filter.setSourceModel(self.mdl)
+		self.view.setModel(self.filter)
+
+		self.view.setRootIsDecorated(False)
+		self.view.setAlternatingRowColors(True)
+		self.view.sortByColumn(0, Qt.AscendingOrder)
+
+		self.edit.textEdited.connect(self._onTextEdited)
+
+		self.crawlTimer = QTimer()
+		self.crawlTimer.timeout.connect(self.doCrawl)
+		self.crawler = None
+
+	@Slot(str)
+	def _onTextEdited(self, text):
+		return self.filter.setFilter(text)
+		parts = map(QRegExp.escape, text)
+		pattern = '.*'.join(parts)
+		self.filter.setFilterRegExp(pattern)
+
+	@Slot()
+	def doCrawl(self):
+		i = 0
+		for path, i in zip(self.crawler, range(self.elements_per_crawl)):
+			self.content.append(path)
+			self.mdl.appendRow(QStandardItem(path))
+
+		if not i:
+			self.crawlTimer.stop()
+
+	def setRoot(self, root):
+		self.root = root
+		self.content = []
+		self.mdl.clear()
+		self.mdl.setHorizontalHeaderLabels([self.tr('File')])
+
+		self.crawler = walk_files(root)
+		self.crawlTimer.start(0)
+
+	# TODO be able to configure crawling: ignored patterns, depth, etc.
+
+
+class FileChooser(BaseFileChooser):
+	def __init__(self, **kwargs):
+		super(FileChooser, self).__init__(**kwargs)
+
+		self.options = PropDict()
+
+		self.edit.textEdited.connect(self._onTextEdited)
+
 		self.view.activated.connect(self._onActivated)
 
 		# models
