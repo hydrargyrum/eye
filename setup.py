3a8a7ce448e4bceed9ff6113d0dafbb0c0754358
Hg <dev@indigo.re>
2017-12-17 00:01:47 +0100

    setup: declare python 3.5 and 3.6 support

--- setup.py
+++ setup.py
@@ -40,6 +40,8 @@ if __name__ == '__main__':
 			'Programming Language :: Python :: 3.2',
 			'Programming Language :: Python :: 3.3',
 			'Programming Language :: Python :: 3.4',
+			'Programming Language :: Python :: 3.5',
+			'Programming Language :: Python :: 3.6',
 		],
 		keywords='code development editor qt script ide',
 
