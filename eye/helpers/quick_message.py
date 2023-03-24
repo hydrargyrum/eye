3c98761f54643a3665cfa8b49c6e728e94413828
Hg <dev@indigo.re>
2023-03-24 13:23:54 +0100

    quick_message: display in focused window

--- eye/helpers/quick_message.py
+++ eye/helpers/quick_message.py
@@ -1,8 +1,10 @@
-from eye.connector import category_objects
+# this project is licensed under the WTFPLv2, see COPYING.txt for details
+
 from eye.utils import ignore_exceptions
+from eye.app import qApp
 
 
 @ignore_exceptions(None)
 def show_quick_message(msg, timeout=0):
-	win = category_objects('window')[0]
+	win = qApp().last_window
 	win.statusBar().showMessage(msg, timeout)
